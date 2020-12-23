2b4e918b80bb5e80642674c66c163346643e8e7b
Mauricio Pardo <mauricio@MacBook-Pro-de-Mauricio.local>
2020-12-22 20:37:39 -0500

    tarea III terminada

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -100,6 +100,7 @@ function promedioResultadosTest(resultadosTest) {
   return t/resultadosTest.length;
 }
 
+
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
